6b462c6178dedf3df6282d9fefca630e6ece4d91
imjinbro <jinbro@likelion.org>
2018-08-07 06:09:15 +0000

    배포자동화 스크립트 수정

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#! /bin/zsh
+JAVA="$JAVA_HOME/java"
+JAVA_OPTS="$JAVA -Djava.security.egd=file:/dev/./urandom"
+
+REPO=$HOME/java-ims
+TARGET=$REPO/build/libs
+
+cd $REPO
+git fetch origin master
+
+origin=$(git rev-parse imjinbro)
+remote=$(git rev-parse origin/imjinbro)
+
+if [[ $origin == $remote ]]; then
+   exit 0
+fi
+
+git merge origin imjinbro
+
+CURRENT_PID=$(pgrep -f java-ims)
+
+if [ -n $CURRENT_PID ]; then 
+    kill -9 $CURRENT_PID
+    sleep 5
+fi
+
+#build gradle
+./gradlew build -x test
+
+if [ ! -d $TARGET ]; then
+    exit 1
+fi
+
+cd $TARGET
+java -jar java-ims-*.jar & > /dev/null 2>&1
+
